7c114603ca07c5b1dfea3b5ef2a54f3dd79ae82b
Just <Just@10.0.0.69>
2016-12-30 19:07:27 -0500

    Updated app

--- Code/dellstore/src/main/java/com/michaelj/dellstore/App.java
+++ Code/dellstore/src/main/java/com/michaelj/dellstore/App.java
@@ -1,17 +1,29 @@
 package com.michaelj.dellstore;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
-public class App 
+@ComponentScan
+@EnableAutoConfiguration
+public class App extends SpringBootServletInitializer
 {
-    public static void main( String[] args )
-    {
-        SpringApplication.run(App.class, args);
+	public static void main(String[] args) {
+        SpringApplication.run(applicationClass, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+
+    private static Class<App> applicationClass = App.class;
 }
